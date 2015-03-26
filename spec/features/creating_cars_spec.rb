614d4992c183a6ec90f34b5cc40659a69c6ecd19
Igor Shcherbak <igorski_is@yahoo.com>
2015-03-25 19:54:33 -0600

    Some changes

--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -30,28 +30,4 @@ feature 'Creating Cars' do
     expect(page).to have_content('2001 Lexus RX300 created')
     expect(page).to have_content('$3,700.00')
   end
-
-  scenario 'can edit a car' do
-    visit '/'
-
-    click_link 'New Car'
-
-    fill_in 'Make', with: 'Ford'
-    fill_in 'Model', with: 'Mustang'
-    fill_in 'Year', with: '1967'
-    fill_in 'Price', with: '2300'
-
-    click_button 'Create Car'
-    
-    click_link 'Edit'
-
-    fill_in 'Model', with: 'Explorer'
-    fill_in 'Year', with: '2012'
-    fill_in 'Price', with: '21200'
-    click_button 'Update Car'
-
-    expect(page).to have_content('2012 Ford Explorer updated')
-    # save_and_open_page
-    expect(page).to have_content('$21,200.00')
-  end
 end
